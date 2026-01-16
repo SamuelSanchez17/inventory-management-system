1799c13d35c5e482f529d9fd1403375951289176
Samuel Sanchez <Samuel.sanchez85@unach.mx>
2026-01-16 08:45:56 -0600

    Agregar archivos de definición de modelos y repositorio para la base de datos

---
+++ src-tauri/src/models.rs
@@ -0,0 +1,42 @@
+//clase para las definiciones de los modelos de la base de datos
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Producto 
+{
+    pub id_producto: i64,
+    pub nombre_producto: String,
+    pub id_categoria: Option<i64>,
+    pub ruta_imagen: Option<String>,
+    pub stock: i64,
+    pub precio: f64,
+    pub creado_at: Option<String>,
+    pub actualizado_at: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Categoria
+{
+    pub id_categoria: i64,
+    pub nombre: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Venta
+{
+    pub id_venta: i64,
+    pub fecha: String,
+    pub total_venta: f64,
+    pub notas: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ProductoVendido
+{
+    pub id_producto_vendido: i64,
+    pub id_venta: i64,
+    pub id_producto: i64,
+    pub cantidad: i64,
+    pub precio_unitario: f64,
+    pub subtotal: f64,
+}
